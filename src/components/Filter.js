169af3a7c7806a964fcead86cb04df4f7cd89792
yaichmohamed <127444596+yaichmohamed@users.noreply.github.com>
2023-05-21 13:24:07 +0200

    Update Filter.js

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,34 +1,41 @@
 import React, { useContext } from 'react';
 import { MovieContext } from '../components/MovieContext';
 
+// Define a functional component called Filter
 const Filter = () => {
-const { titleFilter, ratingFilter, setTitleFilter, setRatingFilter } =
-useContext(MovieContext);
+  // Get the state values and setter functions from MovieContext using useContext
+  const { titleFilter, ratingFilter, setTitleFilter, setRatingFilter } = useContext(MovieContext);
 
-const handleTitleChange = (event) => {
-setTitleFilter(event.target.value);
+  // Event handler for title input change
+  const handleTitleChange = (event) => {
+    setTitleFilter(event.target.value);
+  };
+
+  // Event handler for rating input change
+  const handleRatingChange = (event) => {
+    setRatingFilter(event.target.value);
+  };
+
+  // Render the component
+  return (
+    <div className="filters">
+      {/* Input field for title filter */}
+      <input
+        type="text"
+        placeholder="Title"
+        value={titleFilter}
+        onChange={handleTitleChange}
+      />
+      {/* Input field for rating filter */}
+      <input
+        type="number"
+        placeholder="Minimum Rating"
+        value={ratingFilter}
+        onChange={handleRatingChange}
+      />
+    </div>
+  );
 };
 
-const handleRatingChange = (event) => {
-setRatingFilter(event.target.value);
-};
-
-return (
-<div className="filters">
-<input
-     type="text"
-     placeholder="Title"
-     value={titleFilter}
-     onChange={handleTitleChange}
-   />
-<input
-     type="number"
-     placeholder="Minimum Rating"
-     value={ratingFilter}
-     onChange={handleRatingChange}
-   />
-</div>
-);
-};
-
+// Export the Filter component as the default export
 export default Filter;
