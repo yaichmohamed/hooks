6b08fc356aa79c1a2d32fc4dc9b5dd574c2b3ac4
yaichmohamed <127444596+yaichmohamed@users.noreply.github.com>
2023-05-21 13:25:24 +0200

    Update MovieCard.js

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
+// Define a functional component called MovieCard
 const MovieCard = ({ movie }) => {
+  // Render the movie card
   return (
     <div className="movie-card">
+      {/* Render the movie poster with src and alt from the movie object */}
       <img src={movie.posterURL} alt={movie.title} />
       <div className="movie-info">
+        {/* Render the movie title */}
         <h2>{movie.title}</h2>
+        {/* Render the movie description */}
         <p>{movie.description}</p>
+        {/* Render the movie rating */}
         <p>Rating: {movie.rating}</p>
       </div>
     </div>
   );
 };
 
+// Export the MovieCard component as the default export
 export default MovieCard;
