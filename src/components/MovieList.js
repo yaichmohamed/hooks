20d8db40f878c5970eab08b8d6a5a2b3d3fe22ed
yaichmohamed <127444596+yaichmohamed@users.noreply.github.com>
2023-05-21 13:26:39 +0200

    Update MovieList.js

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import MovieCard from './MovieCard';
 
+// Define a functional component called MovieList
 const MovieList = () => {
+  // Set up state variables using useState
   const [movies, setMovies] = useState([
+    // Initial movie data
     {
       title: 'The Shawshank Redemption',
       description:
@@ -21,34 +24,44 @@ const MovieList = () => {
     },
   ]);
 
+  // Set up state variables for title and rating filters
   const [titleFilter, setTitleFilter] = useState('');
   const [ratingFilter, setRatingFilter] = useState(0);
 
+  // Event handler for title filter change
   const handleTitleFilterChange = (event) => {
     setTitleFilter(event.target.value);
   };
 
+  // Event handler for rating filter change
   const handleRatingFilterChange = (event) => {
     setRatingFilter(event.target.value);
   };
 
+  // Event handler for adding a movie
   const handleAddMovie = (movie) => {
     setMovies([...movies, movie]);
   };
 
+  // Filter the movies based on title and rating filters
   const filteredMovies = movies.filter(
     (movie) =>
       movie.title.toLowerCase().includes(titleFilter.toLowerCase()) &&
       movie.rating >= ratingFilter
-  );return (
+  );
+
+  // Render the component
+  return (
     <div>
       <div className="filters">
+        {/* Input field for title filter */}
         <input
           type="text"
           placeholder="Title"
           value={titleFilter}
           onChange={handleTitleFilterChange}
         />
+        {/* Input field for rating filter */}
         <input
           type="number"
           placeholder="Minimum Rating"
@@ -57,10 +70,12 @@ const MovieList = () => {
         />
       </div>
       <div className="movie-list">
+        {/* Render MovieCard components for each filtered movie */}
         {filteredMovies.map((movie) => (
           <MovieCard key={movie.title} movie={movie} />
         ))}
       </div>
+      {/* Button to add a new movie */}
       <button onClick={() => handleAddMovie({ title: 'New Movie', rating: 0 })}>
         Add Movie
       </button>
@@ -68,4 +83,5 @@ const MovieList = () => {
   );
 };
 
+// Export the MovieList component as the default export
 export default MovieList;
